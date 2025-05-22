11da090cf104b8fc98d1dc6c0e98f5a9eaa5457b
btschneid <btschneid3@gmail.com>
2025-05-21 20:58:35 -0400

    Added cointegrated pair finder

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from src.config import TICKER_SETS, START_DATE, END_DATE, logger
 from src.data_loader import load_data
 from src.preprocess import preprocess_data
+from src.cointegration import find_cointegrated_pairs
 
 def main():
     logger.info("Loading Data...")
     load_data(TICKER_SETS, START_DATE, END_DATE)
 
     logger.info("Cleaning Data...")
-    preprocess_data()
+    processed_data = preprocess_data()
+
+    logger.info("Finding cointegrated pairs...")
+    coint_pairs = find_cointegrated_pairs(processed_data)
 
 if __name__ == "__main__":
     main()
